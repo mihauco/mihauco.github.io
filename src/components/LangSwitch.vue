4054012443140b7bec496ea5aee080c7ef870c46
mihauco <github@mihau.co>
2024-08-21 22:38:43 +0200

    fix nav + lang switcher implementation

---
+++ src/components/LangSwitch.vue
@@ -0,0 +1,64 @@
+<template>
+  <ul class="lang-switch">
+    <li
+      v-for="lang in langs"
+      class="lang-switch__lang"
+      :class="{
+        'lang-switch__lang--active': lang === locale,
+      }"
+    >
+      <span @click="setLocale(lang)">{{ lang }}</span>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+const { locale } = useI18n()
+
+const setLocale = (lang: string) => {
+  locale.value = lang;
+}
+
+const langs = ['pl', 'en']
+</script>
+<style lang="scss">
+.lang-switch {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: flex-end;
+
+  &__lang {
+    padding: 0 8px;
+    position: relative;
+
+    span {
+      cursor: pointer;
+    }
+
+    &--active {
+      span {
+        text-decoration: underline;
+        cursor: default;
+      }
+    }
+
+    &:first-child {
+      padding-left: 0;
+    }
+
+    &:last-child {
+      padding-right: 0;
+    }
+
+    &:not(:last-child)::after {
+      content: '/';
+      position: absolute;
+      right: -5px;
+    }
+  }
+}
+</style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,8 +1,9 @@
 <template>
   <nav class="navigation">
-    <ul>
+    <ul class="navigation__elements">
       <li
         v-for="link in navigationLinks"
+        class="navigation__element"
         :key="link.path"
       >
         <RouterLink :to="link.path">
@@ -10,51 +11,65 @@
         </RouterLink>
       </li>
     </ul>
+    <LangSwitch class="navigation__lang-switch" />
   </nav>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { RouterLink } from 'vue-router';
-import { useI18n } from 'vue-i18n'
 import { navigation } from '@/variables/variables';
-const { t } = useI18n() 
+import { useI18n } from 'vue-i18n';
+import LangSwitch from '@/components/LangSwitch.vue';
+const { t } = useI18n();
 
-const navigationLinks = navigation.map(navigationLink => ({
-  path: navigationLink.path,
-  name: t(`navigation.link.${navigationLink.name}`),
-}))
+const navigationLinks = computed(() => {
+  return navigation.map(navigationLink => ({
+    path: navigationLink.path,
+    name: t(`navigation.link.${navigationLink.name}`),
+  }))
+})
 </script>
 
 <style lang="scss">
-nav {
+.navigation {
   width: var(--navigation-width);
 
-  ul {
+  &__elements {
     list-style: none;
     padding: 0;
     margin: 3px 0 0;
     display: flex;
     flex-direction: column;
     align-items: flex-end;
+  }
 
-    li {
-      margin-bottom: 10px;
-      position: relative;
+  &__element {
+    margin-bottom: 10px;
+    padding-right: 15px;
+    position: relative;
 
-      &:last-child {
-        margin-bottom: 0;
-      }
+    &:last-child {
+      margin-bottom: 0;
+    }
 
+    &:has(.router-link-active) {
       &::after {
-        content: '\00a0\00a0';
-      }
-
-      &:has(.router-link-active) {
-        &::after {
-          content: ' |';
-        }
+        content: '';
+        width: 2px;
+        height: 18px;
+        margin-top: -8px;
+        top: 50%;
+        background-color: var(--font-color-base);
+        position: absolute;
+        right: 0px;
       }
     }
+  }
+
+  &__lang-switch {
+    margin-top: 30px;
+    padding-right: 15px;
   }
 }
 </style>
